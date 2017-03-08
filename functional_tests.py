04bb0899c6d0f9fc2205f2335036e1bc7e04b398
musa <musa@Ronalds-MacBook-Air.local>
2017-03-08 04:09:47 -0600

    end-of-chapter-05

--- functional_tests.py
+++ functional_tests.py
@@ -41,8 +41,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
